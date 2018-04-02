a88ed247a0f7d621d0b9ff21c84d45cc0dba8212
Tanis Pan <e.pan.gar@gmail.com>
2018-04-02 13:42:46 +0200

    1

--- game.js
+++ game.js
@@ -7,10 +7,25 @@ var player = {
   maxWater : 3,     //Maximum water the canteen can hold
   milesTravelled : 0, //Miles travelled
   milesToCamp : 200, //Miles to arrive to camp
-  distFromSmugglers: 0//How far away you are from the smugglers
+  distFromSmugglers: -20//How far away you are from the smugglers
 }
 
 
+
+/*Changes:
+
+A function for different endings? Die of thirst, catched by smugglers, arrive safely...
+Different objects for player and ship?
+An object for nomads?
+A constructor for nomads? (3 different nomads)
+There is a way to ask the player without a message appearing?
+A 1d100 for the random events? (Oasis, nomads, sandstorm)? [x]
+An array for turns?
+Nomad sells: Sandstorm protection shield, turbo, ship cooler, bigger canteen.
+Nomad sells random price?
+Nomad has a minigame for a free price?
+
+*/
 
 player.name =prompt("What's your name?");
 
@@ -18,11 +33,73 @@ alert(player.name +": you were kidnapped three days ago by smugglers during a sp
 alert("Don't run out of water or you'll die of thirst.\nDon't let the smugglers catch you or they will kill you on the spot.\nDon't forget to let your ship cool down once in a while or it will overheat and explode and you'll die in the middle of nowhere.");
 
 function status(){
-  console.log("*************************\n"+"YOUR STATUS:\nName: "+player.name+"\nMiles travelled: "+ player.milesTravelled +"\nMiles to get to your camp: " +(player.milesToCamp-player.milesTravelled)+"\nYour thirst: "+player.thirst+"/6\nWater in your canteen: "+player.water +"/"+player.maxWater+"\nYour ship's heat: "+player.heatShip+"/"+player.maxHeatShip);
+  console.log("*************************\n"+"YOUR STATUS:\nName: "+player.name+"\nMiles travelled: "+ player.milesTravelled +"\nMiles to get to your camp: " +(player.milesToCamp-player.milesTravelled)+"\nYour thirst: "+player.thirst+"/6\nWater in your canteen: "+player.water +"/"+player.maxWater+"\nYour ship's heat: "+player.heatShip+"/"+player.maxHeatShip+"\n*************************");
 }
 
+
+function randomEvent(){
+  result=Math.floor(Math.random()*20)+1;
+
+  if(result==1){
+    console.log("[SANDSTORM()]")
+  }  else if (result == 10){
+    console.log("[FRIENDLY VILLAGE()]")
+  } else if (result>=19) {
+    console.log("[WANDERER()]")
+  } 
+  
+}
+
+
+function drink(){
+    if (player.water > 1){
+    player.water -=1;
+    console.log("You drink from your canteen");
+    } else if (player.water == 1){
+    player.water -=1;
+    console.log("You drink from your canteen.\nYour canteen is empty!");
+    } else {
+      console.log("You can't drink! The canteen is empty!");
+    }
+  
+}
+
+function moderate(){
+  player.milesTravelled+= Math.floor(Math.random()*5)+6;
+  player.thirst+=1;
+  player.heatShip+=1
+  player.distFromSmugglers+= Math.floor(Math.random()*4)+10;
+  console.log("Moderate speed...\n...\n...\n");
+  
+  }
+    
+function fullSpeed(){
+  player.milesTravelled+= Math.floor(Math.random()*6)+10;
+  player.thirst+=1;
+  player.heatShip+=  Math.floor(Math.random()*3)+1;
+  player.distFromSmugglers+= Math.floor(Math.random()*4)+10;
+  console.log(">>>>FULL SPEED!<<<<\n...\n...\n");
+}
+  
+  
+
+
 function ask(){
-  status();
+  status()
+  
+  var answer = prompt("This are your options:\n\nA. Drink from your canteen (Your water reserve will diminish).\nB. Ahead moderate speed (you'll get thirstier, your ship will heat a little).\nC. Ahead full speed (you'll get thirstier, your ship will heat  some more).\nD. Stop to rest.\nR. Restart the game\nQ. Quit the game")
+  if (answer=="a"||answer=="A"){
+    drink();
+  } else if (answer=="b"||answer=="B") {
+    moderate();
+  } else if (answer=="c"||answer=="C") {
+    fullSpeed();
+  } else {
+    alert("Sorry, I don't understand.");
+    ask();
+    
+  } 
+  
   
 }
 

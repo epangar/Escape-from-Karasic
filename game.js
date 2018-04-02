64cd6c603975bf5a69bc5209bf029e5168dc31e6
Tanis Pan <e.pan.gar@gmail.com>
2018-04-02 15:27:38 +0200

    Javascript

--- game.js
+++ game.js
@@ -27,15 +27,23 @@ Nomad has a minigame for a free price?
 
 */
 
+function start(){
 player.name =prompt("What's your name?");
 
 alert(player.name +": you were kidnapped three days ago by smugglers during a space mission to Karasir, a desertic planet. You have managed to escape from their prison, steal some food and a very small, half-broken spaceship. You're trying to get back to your camp, but for that you have to cross the dessert aboard the spaceship.");
 alert("Don't run out of water or you'll die of thirst.\nDon't let the smugglers catch you or they will kill you on the spot.\nDon't forget to let your ship cool down once in a while or it will overheat and explode and you'll die in the middle of nowhere.");
+}
 
 function status(){
   console.log("*************************\n"+"YOUR STATUS:\nName: "+player.name+"\nMiles travelled: "+ player.milesTravelled +"\nMiles to get to your camp: " +(player.milesToCamp-player.milesTravelled)+"\nYour thirst: "+player.thirst+"/6\nWater in your canteen: "+player.water +"/"+player.maxWater+"\nYour ship's heat: "+player.heatShip+"/"+player.maxHeatShip+"\n*************************");
 }
 
+
+function village(){
+  
+  console.log("***You found a village inhabited by friendly people.*** \nThey give you water, refill your canteen and help you cool your ship.");
+  
+}
 
 function randomEvent(){
   result=Math.floor(Math.random()*20)+1;
@@ -51,6 +59,11 @@ function randomEvent(){
 }
 
 
+function checkAll(){
+  
+  randomEvent();
+}
+
 function drink(){
     if (player.water > 1){
     player.water -=1;
@@ -65,35 +78,58 @@ function drink(){
 }
 
 function moderate(){
-  player.milesTravelled+= Math.floor(Math.random()*5)+6;
+  var result=Math.floor(Math.random()*5)+6;
+  player.milesTravelled+= result
   player.thirst+=1;
   player.heatShip+=1
   player.distFromSmugglers+= Math.floor(Math.random()*4)+10;
-  console.log("Moderate speed...\n...\n...\n");
+  console.log("Moderate speed...\n...\n...\nYou traveled "+result+" miles.");
+  checkAll();
   
   }
     
 function fullSpeed(){
-  player.milesTravelled+= Math.floor(Math.random()*6)+10;
+  var result = Math.floor(Math.random()*6)+10;
+  player.milesTravelled+= result;
   player.thirst+=1;
   player.heatShip+=  Math.floor(Math.random()*3)+1;
   player.distFromSmugglers+= Math.floor(Math.random()*4)+10;
-  console.log(">>>>FULL SPEED!<<<<\n...\n...\n");
+  console.log(">>>>FULL SPEED!<<<<\n...\n...\nYou traveled "+result+" miles.");
+  checkAll();
 }
   
-  
+
+function stop(){
+  player.heatShip =0;
+  console.log("You stop to rest.\n\n\n");
+  console.log("You cooled your ship.")
+  player.distFromSmugglers+= Math.floor(Math.random()*4)+10;
+  checkAll();
+
+}
 
 
 function ask(){
   status()
   
-  var answer = prompt("This are your options:\n\nA. Drink from your canteen (Your water reserve will diminish).\nB. Ahead moderate speed (you'll get thirstier, your ship will heat a little).\nC. Ahead full speed (you'll get thirstier, your ship will heat  some more).\nD. Stop to rest.\nR. Restart the game\nQ. Quit the game")
+  var answer = prompt("This are your options:\n\nA. Drink from your canteen (your water reserve will diminish).\nB. Ahead moderate speed (you'll get thirstier, your ship will heat a little).\nC. Ahead full speed (you'll get thirstier, your ship will heat  some more).\nD. Stop to rest (your ship will cool down).\nR. Restart the game\nQ. Quit the game")
   if (answer=="a"||answer=="A"){
     drink();
+    ask();
   } else if (answer=="b"||answer=="B") {
     moderate();
+    ask();
   } else if (answer=="c"||answer=="C") {
     fullSpeed();
+    ask();
+  } else if (answer=="d"||answer=="D") {
+    stop();
+    ask();
+  } else if (answer=="r"||answer=="R") {
+    game();
+  }  else if (answer=="q"||answer=="Q") {
+    console.log("Bye!");
+    return "The End";
   } else {
     alert("Sorry, I don't understand.");
     ask();
@@ -103,4 +139,9 @@ function ask(){
   
 }
 
-ask();
+function game(){
+  start();
+  ask();
+  
+}
+game();
